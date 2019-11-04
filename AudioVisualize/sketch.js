a3db5ff6db08d02781fedfa3154ec4761a6b94b4
RuoqiLi <lir802@newschool.edu>
2019-11-03 19:59:17 -0500

    Audio Visualize

--- AudioVisualize/sketch.js
+++ AudioVisualize/sketch.js
@@ -2,12 +2,13 @@ var fft,pieces,radius;
 let image;
 var imgurl;
 var link="http://www.colr.org/json/schemes/random/7";
-var colorPalette = ["rgba(20, 20, 70, 0.05)", "rgba(40, 70, 72, 0.5)", "#00a6e0", "#00f9d9"];
+var colorPalette = ["rgba(20, 20, 70, 0.05)", "rgba(40, 70, 72, 0.5)", "#00a6e0", "#00f9d9","#0034d9"];
 function preload(){
-  sound = loadSound('Assets/DEMO_1.mp3');
+  sound = loadSound('Assets/DEMO_5.mp3');
 }
 
 function setup(){
+
   let cnv = createCanvas(500,500);
   //click to start
   cnv.mouseClicked(togglePlay);
@@ -29,26 +30,35 @@ function draw(){
   var mapBass     = map( bass, 0, 255, -100, 100 );
   var mapMid      = map( mid, 0, 255, -150, 150 );
   var mapTreble   = map( treble, 0, 255, -200, 200 );
+  var scaleTreble = map(treble, 0, 255, 1, 1.5);
   pieces= map(mouseX, 0, width, 5, 20);
   radius= map(mouseY, 0, height, 5, 300);
   
   let spectrum = fft.analyze();
-  wave();
+  //wave();
 
     translate( width/2, height/2 )
   for( i = 0; i < pieces; i++ ) {
     
     // Rotate the point of origin
     rotate(TWO_PI / pieces)
-    
+    /*--  BASS --*/
     push();
-    strokeWeight(2);
+    strokeWeight(3);
     stroke(colorPalette[3]);
     line( mapBass, radius*1, 0, radius*1);
-    
+    /*--  Mid --*/
     strokeWeight(0.5);
     stroke(colorPalette[2]);
     line( mapMid, radius*0.7,0, radius ); 
+    /*--  TREMBLE --*/
+    push();
+    strokeWeight(0.01);
+
+    stroke(colorPalette[4]);
+    scale(scaleTreble);
+    line(mapTreble, radius / 3, radius, radius);
+    line(-mapTreble, -radius / 3, radius, radius);
   }
   
   
@@ -62,9 +72,11 @@ function draw(){
 function togglePlay() {
   if (sound.isPlaying()) {
     sound.pause();
-    loadJSON(link,gotcolor);
+    
   } else {
     sound.loop();
+    loadJSON(link,gotcolor);
+    console.log(link);
   }
 }
 function wave(){
@@ -86,6 +98,7 @@ let waveform = fft.waveform();
 function drawspectrum(spectrum){
   
   stroke(colorPalette[1]);
+  strokeWeight(0.5);
   fill(0,0); // spectrum is green
   for (var i = 0; i< spectrum.length; i++){
     let x = map(i, 0, spectrum.length, 0, width);
@@ -96,5 +109,10 @@ function drawspectrum(spectrum){
 
 }
 function gotcolor(data){
-console.log(data.schemes[1].id);
+console.log(data.schemes[1].colors[1]);
+console.log(data.schemes[1].colors[2]);
+colorPalette[2]="#"+data.schemes[1].colors[1];
+colorPalette[3]="#"+data.schemes[1].colors[2];
+colorPalette[4]="#"+data.schemes[1].colors[3];
+console.log(colorPalette[3]);
 }
